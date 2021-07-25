4350fb572c2985b284edc7531004c1a596c9d4b7
baboures <jpablos99@gmail.com>
2021-07-25 23:06:39 +0200

    Update Autoload.php

--- src/Autoload.php
+++ src/Autoload.php
@@ -6,3 +6,6 @@ require_once __DIR__ . '/Request.php';
 require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/Component.php';
 require_once __DIR__ . '/Basic.php';
+require_once __DIR__ . '/Exception.php';
+require_once __DIR__ . '/PHPMailer.php';
+require_once __DIR__ . '/SMTP.php';
